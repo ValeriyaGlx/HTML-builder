29eecf8e34f58abe1838e9de18f02567e4df4fcb
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-05-07 22:55:12 +0400

    feat: solve 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs");
+const path = require("path");
 
+fs.mkdir(path.join(__dirname, "files-copy"), {recursive: true} , (err) => {
+  if (err) throw err;
+});
+
+fs.promises
+  .readdir(path.join(__dirname, "files"),(err) => {
+    if (err) throw err;
+  })
+  .then((files) => {
+    for (let file of files) {
+      fs.copyFile(
+        path.join(__dirname, "files", file),
+        path.join(__dirname, "files-copy", file),
+        (err) => {
+          if (err) throw err;
+          console.log("File successfully copied");
+        }
+      );
+    }
+  });
