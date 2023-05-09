70762e7cff00ea645c5a77a4777d3d4d1b5f3410
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-05-09 22:26:53 +0400

    fix: fix 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,17 @@ fs.mkdir(path.join(__dirname, "files-copy"), {recursive: true} , (err) => {
   if (err) throw err;
 });
 
+    fs.promises.readdir(path.join(__dirname, 'files-copy'), (err) => {})
+    .then((files) =>{
+      for (let file of files) {
+        let newFile = path.join(path.join(__dirname, 'files-copy'), file);
+        fs.unlink(newFile, (err) => {
+          if (err) {
+
+          }})}
+    }
+    )
+
 fs.promises
   .readdir(path.join(__dirname, "files"),(err) => {
     if (err) throw err;
@@ -20,4 +31,6 @@ fs.promises
         }
       );
     }
-  });
+  
+  })
+
